540e3b2dccdcb4de705a22c1af5ec5e20e4ee504
Ariel Isaacs <ariel.isaacs@gmail.com>
2015-01-19 11:44:54 -0800

    add files

--- lib/model.rb
+++ lib/model.rb
@@ -1,8 +1,17 @@
 class Restaurant < ActiveRecord::Base
 	# has_many :reviews
+
+	def self.make_restaurants
+		r1= Restaurant.new(:name => "Simple Pleasures", :rating=>[5])
+		r1.save
+		r2 = Restaurant.new(:name => "Cafe Promenade", :rating=>[4])
+		r2.save
+	end
 end
 
 class Review < ActiveRecord::Base
 	# belongs_to :restaurant
 end
+
+
 
